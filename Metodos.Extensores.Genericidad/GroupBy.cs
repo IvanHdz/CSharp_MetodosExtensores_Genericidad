476be7f094c2585c7bd7da39e36232944e220b70
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-09-03 17:12:11 -0500

    Commit

---
+++ Metodos.Extensores.Genericidad/GroupBy.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Metodos.Extensores.Genericidad
+{
+    #region GroupBy Iteradores + Genericidad EJEMPLO de Query methods usando iteradores y genericidad
+
+    //Delegate genérico. Métodos que reciben un parámetro de tipo T y devuelven un valor de tipo R
+    public delegate R Func<T, R>(T x);
+
+    #region Group
+
+    public class Group<K, T> : IEnumerable<T>
+    {
+        private readonly K key;
+        private IEnumerable<T> g;
+
+        public Group(K groupKey, IEnumerable<T> g)
+        {
+            this.key = groupKey;
+            this.g = g;
+        }
+
+        public K Key { get { return key; } }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return g.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    #endregion Group
+
+    public static class Query
+    {
+        #region GROUP BY method. Iterador sobre Iterador
+
+        /*
+           * NO es una implementación eficiente, solo como ejemplo
+           * Dado un selector que a partir de un T obtiene un K y un IEnumerable de T, devuelve un IEnumerable de grupos segun el K
+           * y todos los T del IEnumerable que al aplicar el selector se obtiene K.
+           * Admirar la belleza del efecto lazy del yield return
+        */
+
+        public static IEnumerable<Group<K, T>> GroupBy<T, K>(this IEnumerable<T> source,
+                                                             Func<T, K> selector)
+        {
+            List<K> processedKeys = new List<K>();
+            foreach (T t in source)
+            {
+                K key = selector(t);
+                if (!processedKeys.Contains(key))
+                {
+                    processedKeys.Add(key);
+                    yield return new Group<K, T>(key, Grouping(source, selector, key));
+                }
+            }
+        }
+
+        private static IEnumerable<T> Grouping<T, K>(IEnumerable<T> source,
+                                             Func<T, K> selector, K key)
+        {
+            foreach (T t in source)
+                if (selector(t).Equals(key)) yield return t;
+        }
+
+        #endregion GROUP BY method. Iterador sobre Iterador
+    }
+
+    #endregion GroupBy Iteradores + Genericidad EJEMPLO de Query methods usando iteradores y genericidad
+}
---
+++ Metodos.Extensores.Genericidad/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Metodos.Extensores.Genericidad
+{
+    internal delegate bool Predicado<T>(T x);
+
+    internal static class MisMetodos
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> items, Predicado<T> filtro)
+        {
+            foreach (T x in items)
+                if (filtro(x)) yield return x;
+        }
+
+        public static T Mayor<T>(this IEnumerable<T> items) where T : IComparable
+        {
+            T max = default(T);
+            bool empty = true;
+            foreach (T x in items)
+            {
+                max = x; empty = false; break;
+            }
+            if (empty) throw new Exception("The source cannot be empty");
+            foreach (T x in items)
+            {
+                if (x.CompareTo(max) > 0)
+                    max = x;
+            }
+            return max;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> colores = new List<string> { "amarillo", "verde","rojo",  "azul", "blanco", "negro", "naranja",
+                                                      "mostaza", "rosa", "gris", "morado", "marrón", "violeta"};
+            int[] numeros = { 10, -60, 20, -50, 30, -40 };
+            Console.WriteLine("\nColores de nombre corto");
+            foreach (string s in MisMetodos.Where(colores, x => x.Length <= 5))
+                Console.WriteLine(s);
+            Console.WriteLine("\nPositivos");
+            foreach (int i in MisMetodos.Where(numeros, x => x > 0))
+                Console.WriteLine(i);
+
+            Console.WriteLine("\nDe los colores de nombre corto el mayor alfabéticamente");
+            Console.WriteLine(MisMetodos.Mayor(MisMetodos.Where(colores, x => x.Length <= 5)));
+
+            #region Con notación punto usando extensores
+
+            //Convertir en extensores los métodos poniendo el this
+            //Mostrar el uso del Intellisense
+
+            Console.WriteLine("\nColores de nombre corto");
+            foreach (string s in colores.Where(x => x.Length <= 5))
+                Console.WriteLine(s);
+            Console.WriteLine("\nNegativos");
+            foreach (int i in numeros.Where(x => x < 0))
+                Console.WriteLine(i);
+
+            Console.WriteLine("\nDe los colores de nombre largo el mayor alfabéticamente");
+            Console.WriteLine(colores.Where(x => x.Length > 5).Mayor());
+
+            Console.WriteLine("\nDe los negativos el mayor");
+            Console.WriteLine(numeros.Where(x => x < 0).Mayor());
+
+            #endregion Con notación punto usando extensores
+
+            #region Uso del GroupBy Ejemplo de inferencia de tipo
+
+            var coloresAgrupados = colores.GroupBy(s => s.Length);
+            Console.WriteLine("\nGrupos de colores según longitud");
+            foreach (var g in coloresAgrupados)
+            {
+                Console.WriteLine(g.Key);
+                foreach (var s in g)
+                    Console.WriteLine("  {0}", s);
+            }
+
+            //Más declarativo
+            var coloresPorLetra = from c in colores group c by c[0];
+            Console.WriteLine("\nGrupos de colores según primera letra");
+            foreach (var g in coloresPorLetra)
+            {
+                Console.WriteLine(g.Key);
+                foreach (var s in g)
+                    Console.WriteLine("  {0}", s);
+            }
+
+            #endregion Uso del GroupBy Ejemplo de inferencia de tipo
+
+            Console.ReadLine();
+        }
+    }
+}
